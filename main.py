dc39ce79f5eb99c1b9e77041013702703c01fd5c
SAMIP <023a420@sxc.edu.np>
2026-01-20 00:48:40 +0545

    created fast api skeleton

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI(title="Progress Vault")
+
+# Setup Templates (HTML)
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "message": "Welcome to the Vault"})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
